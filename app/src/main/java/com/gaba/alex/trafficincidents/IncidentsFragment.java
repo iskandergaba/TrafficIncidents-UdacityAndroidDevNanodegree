8b853e252c1e040a405a9d9f6d237b1531b56b31
Alex Gaba <iskandergaba@gmail.com>
2016-08-07 00:00:06 +0100

    Optimised app dependencies and added a Place Picker functionality

--- app/src/main/java/com/gaba/alex/trafficincidents/IncidentsFragment.java
+++ app/src/main/java/com/gaba/alex/trafficincidents/IncidentsFragment.java
@@ -1,7 +1,7 @@
 package com.gaba.alex.trafficincidents;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +17,6 @@ public class IncidentsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_incidents, container, false);
 
     }
--- app/src/paid/java/com/gaba/alex/trafficincidents/paid/MainActivity.java
+++ app/src/paid/java/com/gaba/alex/trafficincidents/paid/MainActivity.java
@@ -1,18 +1,32 @@
 package com.gaba.alex.trafficincidents.paid;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.gaba.alex.trafficincidents.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private Intent mPlacePickerIntent;
+    private final int PLACE_PICKER_REQUEST = 1;
+    private final String PREF_LAT = "lat";
+    private final String PREF_LNG = "lng";
+    private final String PREF_ADDRESS = "address";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +34,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String address = preferences.getString(PREF_ADDRESS, "Tap to choose a location");
+        TextView addressTextView = (TextView)findViewById(R.id.address);
+        addressTextView.setText(address);
+        if (preferences.contains(PREF_LAT) && preferences.contains(PREF_LNG)) {
+            Double lat = Double.parseDouble(preferences.getString(PREF_LAT, "0"));
+            Double lng = Double.parseDouble(preferences.getString(PREF_LNG, "0"));
+            builder.setLatLngBounds(new LatLngBounds(new LatLng(lat, lng), new LatLng(lat, lng)));
+        }
+
+        try {
+            mPlacePickerIntent = builder.build(this);
+        } catch (GooglePlayServicesNotAvailableException | GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivityForResult(mPlacePickerIntent, PLACE_PICKER_REQUEST);
             }
         });
+
     }
 
     @Override
@@ -50,5 +81,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place selectedPlace = PlacePicker.getPlace(data, this);
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(PREF_LAT, selectedPlace.getLatLng().latitude + "");
+                editor.putString(PREF_LNG, selectedPlace.getLatLng().longitude + "");
+                editor.putString(PREF_ADDRESS, selectedPlace.getAddress().toString());
+                editor.apply();
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                Double lat = Double.parseDouble(preferences.getString(PREF_LAT, "0"));
+                Double lng = Double.parseDouble(preferences.getString(PREF_LNG, "0"));
+                String address = preferences.getString(PREF_ADDRESS, "Location");
+                TextView addressTextView = (TextView)findViewById(R.id.address);
+                addressTextView.setText(address);
+                builder.setLatLngBounds(new LatLngBounds(new LatLng(lat, lng), new LatLng(lat, lng)));
+                try {
+                    mPlacePickerIntent = builder.build(this);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
