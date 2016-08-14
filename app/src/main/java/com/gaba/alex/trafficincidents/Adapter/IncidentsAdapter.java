daa25784761fa9acecf9235cb92e9318ab218341
Alex Gaba <iskandergaba@gmail.com>
2016-08-14 18:00:40 +0100

    User interface improvements and added a license to the app

--- app/src/main/java/com/gaba/alex/trafficincidents/Adapter/IncidentsAdapter.java
+++ app/src/main/java/com/gaba/alex/trafficincidents/Adapter/IncidentsAdapter.java
@@ -1,9 +1,25 @@
-package com.gaba.alex.trafficincidents.Adapter;
+/*
+Copyright 2016 Iskander Gaba
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.gaba.alex.trafficincidents.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +32,6 @@ import com.gaba.alex.trafficincidents.R;
 import com.gaba.alex.trafficincidents.Utility;
 
 import java.text.DateFormat;
-import java.text.MessageFormat;
 import java.util.Date;
 
 public class IncidentsAdapter extends SimpleCursorAdapter {
@@ -47,7 +62,7 @@ public class IncidentsAdapter extends SimpleCursorAdapter {
         final double lng = cursor.getDouble(cursor.getColumnIndexOrThrow(IncidentsColumns.LNG));
         final long dateInMillis = Long.parseLong(cursor.getString(cursor.getColumnIndexOrThrow(IncidentsColumns.END_DATE)));
         final boolean roadClosed = Boolean.valueOf(cursor.getString(cursor.getColumnIndexOrThrow(IncidentsColumns.ROAD_CLOSED)));
-        final String description = mContext.getString(R.string.local_description) + ": " + cursor.getString(cursor.getColumnIndexOrThrow(IncidentsColumns.DESCRIPTION));
+        final String description = cursor.getString(cursor.getColumnIndexOrThrow(IncidentsColumns.DESCRIPTION));
 
 
         TextView typeTextView = (TextView) view.findViewById(R.id.incident_type);
@@ -58,26 +73,37 @@ public class IncidentsAdapter extends SimpleCursorAdapter {
         TextView severityTextView = (TextView) view.findViewById(R.id.incident_severity);
         if (severityTextView != null) {
             severityTextView.setText(Utility.getIncidentSeverity(mContext, severity));
+            severityTextView.setTextColor(ContextCompat.getColor(mContext, Utility.getIncidentColor(severity)));
         }
 
         TextView roadClosedTextView = (TextView) view.findViewById(R.id.incident_road_closed);
         if (roadClosedTextView != null) {
-            roadClosedTextView.setText(MessageFormat.format("{0}{1}",
-                    mContext.getString(R.string.road_closed) + ": ",
-                    roadClosed ? mContext.getString(R.string.yes) : mContext.getString(R.string.no)));
+            roadClosedTextView.setText(String.format("%s: ", mContext.getString(R.string.road_closed)));
         }
 
-        TextView dateTextView = (TextView) view.findViewById(R.id.incident_update);
+        TextView roadClosedContentTextView = (TextView) view.findViewById(R.id.incident_road_closed_content);
+        if (roadClosedContentTextView != null) {
+            roadClosedContentTextView.setText(roadClosed ? mContext.getString(R.string.yes) : mContext.getString(R.string.no));
+        }
+
+        TextView dateTextView = (TextView) view.findViewById(R.id.incident_end_date);
         if (dateTextView != null) {
-            Date date = new Date(dateInMillis);
-            DateFormat formatter = DateFormat.getInstance();
-            String dateStr = mContext.getString(R.string.estimated_end_date) + ": " + formatter.format(date);
-            dateTextView.setText(dateStr);
+            dateTextView.setText(String.format("%s: ", mContext.getString(R.string.estimated_end_date)));
+        }
+
+        TextView dateContentTextView = (TextView) view.findViewById(R.id.incident_end_date_content);
+        if (dateContentTextView != null) {
+            dateContentTextView.setText(DateFormat.getDateInstance().format(new Date(dateInMillis)));
         }
 
         TextView descriptionTextView = (TextView) view.findViewById(R.id.incident_description);
         if (descriptionTextView != null) {
-            descriptionTextView.setText(description);
+            descriptionTextView.setText(String.format("%s: ", mContext.getString(R.string.local_description)));
+        }
+
+        TextView descriptionContentTextView = (TextView) view.findViewById(R.id.incident_description_content);
+        if (descriptionContentTextView != null) {
+            descriptionContentTextView.setText(description);
         }
 
         ImageView showOnMapImageView = (ImageView) view.findViewById(R.id.show_on_map_button);
@@ -85,8 +111,8 @@ public class IncidentsAdapter extends SimpleCursorAdapter {
             @Override
             public void onClick(View view) {
                 Intent intent = Utility.buildShowOnMapIntent(lat, lng);
-                if (intent.resolveActivity(context.getPackageManager()) != null) {
-                    context.startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+                    mContext.startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
                 }
             }
         });
@@ -97,10 +123,10 @@ public class IncidentsAdapter extends SimpleCursorAdapter {
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("text/plain");
-                String shareText = Utility.getIncidentType(mContext, type) + "\n\n" + description + "\n\n"
-                        + mContext.getString(R.string.severity) + ": " + Utility.getIncidentSeverity(mContext, severity)
-                        + "\n\n" + mContext.getString(R.string.road_closed) + ": "
-                        + (roadClosed ?  mContext.getString(R.string.yes) : mContext.getString(R.string.no))
+                String shareText = Utility.getIncidentType(mContext, type)
+                        + "\n\n" + mContext.getString(R.string.local_description) + ": " + description
+                        + "\n\n" + mContext.getString(R.string.severity) + ": " + Utility.getIncidentSeverity(mContext, severity)
+                        + "\n\n" + mContext.getString(R.string.road_closed) + ": " + (roadClosed ?  mContext.getString(R.string.yes) : mContext.getString(R.string.no))
                         + "\n\n" + mContext.getString(R.string.provided_by) + ": " + mContext.getString(R.string.app_name);
                 intent.putExtra(Intent.EXTRA_TEXT, shareText);
                 if (intent.resolveActivity(mContext.getPackageManager()) != null) {
--- app/src/main/java/com/gaba/alex/trafficincidents/Utility.java
+++ app/src/main/java/com/gaba/alex/trafficincidents/Utility.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2016 Iskander Gaba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.gaba.alex.trafficincidents;
 
 import android.app.ActivityManager;
@@ -78,6 +94,11 @@ public class Utility {
     public static String getIncidentSeverity(Context context, int severity) {
         final String[] severityCodes = context.getResources().getStringArray(R.array.incidentSeverityCodes);
         return severityCodes[severity - 1];
+    }
+
+    public static int getIncidentColor(int type) {
+        int[] typeColors = {R.color.low_impact, R.color.minor, R.color.moderate, R.color.serious};
+        return typeColors[type - 1];
     }
 
     public static Intent buildShowOnMapIntent(double lat, double lng) {
